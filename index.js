a209189ca0baf6ab34ae829231de58526d38b952
Federico Garcia Macchioni <fedem_04@hotmail.com>
2023-12-10 12:02:53 -0300

    solo consola

--- index.js
+++ index.js
@@ -1,124 +1,46 @@
-const cuadrado = document.querySelector(".cuadrado");
-let cuadradoColores = true;
-
-const numeros = document.querySelector(".numeros");
-
-const suma1 = document.querySelector(".suma1");
-const suma2 = document.querySelector(".suma2");
-const resta1 = document.querySelector(".resta1");
-const resta2 = document.querySelector(".resta2");
-const resultadoSuma = document.querySelector(".resultadoSuma");
-const resultadoResta = document.querySelector(".resultadoResta");
+let numeroWhile = 0;
+let si = true;
+const nombre = prompt("Ingrese nombre");
 
 
 // if //
-function cuadradoColor() {
+function ifsi() {
 
-  if (cuadradoColores === true) {
-    cuadrado.classList.remove("cuadradoRojo");
-    cuadrado.classList.add("cuadradoAmarillo");
-    console.log("cuadrado amarillo")
-    cuadradoColores = false;
+  if (si === true) {
+    console.log("if -> ", "si")
+    si = false;
   }
 
-  else if (cuadradoColores === false) {
-    cuadrado.classList.remove("cuadradoAmarillo");
-    cuadrado.classList.add("cuadradoRojo");
-    console.log("cuadrado rojo")
-    cuadradoColores = true;
-
+  else if (si === false) {
+    console.log("if -> ", "no")
+    si = true;
   }
 
   else return;
 }
-
+ifsi();
 
 // for //
-function suma() {
+function bucle() {
   let i = 0;
-  for (i = 0; i <= 0; i++) {
-    numeros.textContent++;
-    console.log(numeros.textContent);
+  for (i = 0; i <= 10; i++) {
+    numero = i
+    console.log("for -> ", numero);
   }
 }
+bucle();
 
-function resta() {
-  let i = 0;
-  for (i = 0; i >= 0; i--) {
-    numeros.textContent--;
-    console.log(numeros.textContent);
-  }
+// while //
+while (numeroWhile < 5) {
+  numeroWhile++;
 }
-console.log(numeros.textContent);
+console.log("while -> ", numeroWhile)
 
-
-
-// que el usuario mande un dato y nuestro codigo pueda hacer algo //
-function sumar() {
-  let resultado = suma1.valueAsNumber + suma2.valueAsNumber;
-  resultadoSuma.innerHTML = resultado;
-  if (resultado <= 0 || resultado >= 0) {
-    console.log(suma1.valueAsNumber + suma2.valueAsNumber);
-    // alert(resultado);
-    return resultado;
-  }
-
-  else if (suma1.valueAsNumber <= 0 || suma1.valueAsNumber >= 0) {
-    console.log(suma1.valueAsNumber + " + 0 = " + suma1.valueAsNumber)
-    return resultadoSuma.innerHTML = suma1.valueAsNumber;
-  }
-  else if (suma2.valueAsNumber <= 0 || suma2.valueAsNumber >= 0) {
-    console.log("0 + " + suma2.valueAsNumber + " = " + suma2.valueAsNumber)
-    return resultadoSuma.innerHTML = suma2.valueAsNumber;
-  }
-  else {
-    console.log("0")
-    return resultadoSuma.innerHTML = "0";
-  }
-
-
+// prompt //
+function saludar(nombre) {
+  console.log("Buenos dias " + nombre + "!");
 }
-
-function restar() {
-
-  let resultado = resta1.valueAsNumber - resta2.valueAsNumber;
-  resultadoResta.innerHTML = resultado;
-
-
-  if (resultado <= 0 || resultado >= 0) {
-    console.log(resta1.valueAsNumber - resta2.valueAsNumber)
-    return resultado;
-  }
-  else if (resta1.valueAsNumber <= 0 || resta1.valueAsNumber >= 0) {
-    console.log(resta1.valueAsNumber + " - 0 = " + resta1.valueAsNumber)
-    return resultadoResta.innerHTML = resta1.valueAsNumber;
-  }
-  else if (resta2.valueAsNumber <= 0 || resta2.valueAsNumber >= 0) {
-    console.log("0 - " + resta2.valueAsNumber + " = " + resta2.valueAsNumber)
-    return resultadoResta.innerHTML = resta2.valueAsNumber;
-  }
-  else {
-    console.log("0")
-    return resultadoResta.innerHTML = "0";
-  }
-}
-
-function reset() {
-  if (cuadradoColores === false) {
-    cuadrado.classList.remove("cuadradoAmarillo");
-    cuadrado.classList.add("cuadradoRojo");
-    cuadradoColores = true;
-  }
-  numeros.innerHTML = "0";
-  suma1.value = "";
-  suma2.value = "";
-  resta1.value = "";
-  resta2.value = "";
-  resultadoSuma.innerHTML = "";
-  resultadoResta.innerHTML = "";
-  console.clear();
-}
-
+saludar(nombre)
 
 
 
